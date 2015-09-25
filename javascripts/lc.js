b153f0c31573ac5ae538042442b1c86dacf41276
unknown <Administrator@happigo-zhangr.bj.happigo.com>
2015-09-25 11:18:17 +0800

    新增kindeditor

--- javascripts/lc.js
+++ javascripts/lc.js
@@ -1,5 +1,5 @@
-var indexConfig=[{"name":"aa","cnName":"dd"},{"name":"bb","cnName":"ee"}];
-
+var indexConfig=[{"cnName":"json 美化工具","ding":"100","url":"jsonFormatUtil.html"}];
+var ynConfig=[{"cnName":"kindeditor 与 easyUI结合扩展上传图片 按钮 ","ding":"1","url":"easyUiKinderEditor.html"}];
 
 function initTables(tmpId,obj){
 	//#name
